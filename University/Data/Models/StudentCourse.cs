46a009c009df5a893904c9760e0085a38274d2e4
NickolayMamonov <n.mamonovv@gmail.com>
2021-11-12 15:48:53 +0300

    Update database

---
+++ University/Data/Models/StudentCourse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Collections.Generic;
+namespace University.Data.Models
+{
+    public class StudentCourse
+    {
+        [ForeignKey("Student")] public int StudentId { get; set; }
+        public virtual Student Student { get; set; }
+
+        [ForeignKey("Course")] public int CourseId { get; set; }
+        public virtual Course Course { get; set; }
+    }
+}
